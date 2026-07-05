c5ef34fc0e369c3fb5d9fe9ad427852338c0bd80
nastyaparfyonova97 <nastya.parfyonova.97@gmail.com>
2026-07-05 23:14:16 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,18 @@
+import os
 
+# Пути к папкам
+DATA_DIR = "data"
+REPORTS_DIR = "reports"
+LOGS_DIR = "logs"
+
+# Настройки для фильтрации
+STATUS_COLUMN = "status"
+DELIVERED_STATUS = "Delivered"
+
+# Настройки для отчета
+REPORT_FILENAME = "summary_report.csv"  # Имя выходного файла
+ERROR_LOG_FILENAME = "errors.log"
+
+# Создание необходимых папок, если они не существуют
+for directory in [DATA_DIR, REPORTS_DIR, LOGS_DIR]:
+    os.makedirs(directory, exist_ok=True)
